1badc7f8ff9b6288068589e8bcfd6788d70e6504
Vladimir Kushnerov <lelik.krovo@gmail.com>
2021-04-17 21:16:10 +0300

    test/ while

--- test.py
+++ test.py
@@ -1,3 +1,15 @@
-a = [3,4,3,3,5,4,6,3]
-while 3 in a:
-    a.remove(3)
+#a = int(input())
+#b = int(input())
+#print(f'{a} + {b} = {a+b}')
+
+#x, y = int(input()), int(input())
+#y = y % x
+#print(x - y)
+
+#x = int(input())
+#s = 100000 * (1 + 10) ** x
+#print(round(s))
+
+x = int(input())
+s = 100000 * (1 + 10/100) ** x
+print(round(s))
